c0b524dadee85b5c592e88a7d93bc853223c2aba
Ribeiro <joao.ribeiro17@outlook.com>
2024-04-04 00:29:01 -0300

    Passos iniciais para gerador de senha

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import Slider from '@react-native-community/slider'
 
-export default function App() {
-  return (
+
+export default function App(){
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <Image 
+        source={require("./src/assets/logo.png")}
+        style={styles.logo}
+      />
+
+      <Text style={styles.title}>20 Caracteres</Text>
+
+      <View style={styles.area}>
+        <Slider
+          style={{ height:50 }}
+          minimumValue={6}
+          maximumValue={20}
+          maximumTrackTintColor='ff4444'
+          minimumTrackTintColor='#ff0000'
+          thumbTintColor='#ECD172'
+        />
+      </View>
+
+      </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: "#f3f3f3",
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  logo:{
+    marginBottom: 60,
+    width: 150,
+    height: 150
+  },
+  area:{
+    marginTop: 14,
+    marginBottom: 14,
+    width: "80%",
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    
+  }
+}) 
