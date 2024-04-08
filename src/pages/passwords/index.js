6343021374611af6fbb1a96ca5976f139d21938b
Ribeiro <joao.ribeiro17@outlook.com>
2024-04-07 23:51:43 -0300

    Adicionando senhas a pagina de senhas

--- src/pages/passwords/index.js
+++ src/pages/passwords/index.js
@@ -1,16 +1,38 @@
 import { useState, useEffect} from 'react'
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {useIsFocused} from '@react-navigation/native'
+import useStorage from '../../hooks/useStorage'
 
 export function Passwords(){
-const [listPassword, setListPasswords] = useState([])
+const [listPasswords, setListPasswords] = useState([])
+const focused = useIsFocused();
+const { getItem } = useStorage();
+
+
+useEffect(() => {
+    async function loadPasswords(){
+        const passwords = await getItem("@pass")
+        setListPasswords(passwords);
+    }
+
+    loadPasswords();
+}, [focused])
 
     return(
         <SafeAreaView style={{ flex:1,}}>
             <View style={styles.header}>
                 <Text style={styles.title}>Minhas senhas</Text>
             </View>
+
+            <View style={styles.content}>
+                <FlatList 
+                 data={listPasswords}
+                 keyExtractor={ (item) => String(item) }
+                 renderItem={ ({ item }) => <Text>{item}</Text>}
+                />
+            </View>
+
         </SafeAreaView>
     )
 }
